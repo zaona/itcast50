a848bd767cae198ea4845dcd824806db5e35c7c2
zaona <1641903820@qq.com>
2018-09-03 21:27:27 +0800

    新建Home组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/views/login'
+import Home from '@/views/Home'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {name: 'home', path: '/', ridirect: {name: 'login'}},
-    {name: 'login', path: '/login', component: Login}
+    {name: 'login', path: '/login', component: Login},
+    {name: 'home', path: '/home', component: Home}
   ]
 })
--- src/views/login.vue
+++ src/views/login.vue
@@ -5,9 +5,9 @@
             <el-input v-model="formData.username"></el-input>
         </el-form-item>
         <el-form-item label="密码">
-            <el-input v-model="formData.password"></el-input>
+            <el-input type="password" v-model="formData.password"></el-input>
         </el-form-item>
-        <el-button type="primary" class="login-btn">登录</el-button>
+        <el-button type="primary" class="login-btn" @click="handleLogin">登录</el-button>
         </el-form>
   </div>
 </template>
@@ -20,6 +20,26 @@ export default {
         username: '',
         password: ''
       }
+    }
+  },
+  methods: {
+    handleLogin () {
+      this.$http
+        .post('login', this.formData)
+        .then((result) => {
+          const data = result.data
+          const {meta: {status, msg}} = data
+          if (status === 200) {
+            const token = data.data.token
+            sessionStorage.setItem('token', token)
+            this.$message.success(msg)
+          } else {
+            this.$message.console.error(msg)
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
     }
   }
 }
