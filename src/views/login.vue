a139882454048338a2d4dd9651382b0340719fd2
zaona <1641903820@qq.com>
2018-09-03 20:04:36 +0800

    样式调整

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <el-form label-position="top" label-width="80px" :model="formData">
+    <div class="login-wrap">
+        <el-form label-position="top" label-width="80px" :model="formData" class="login-form">
         <el-form-item label="用户名">
             <el-input v-model="formData.username"></el-input>
         </el-form-item>
         <el-form-item label="密码">
             <el-input v-model="formData.password"></el-input>
         </el-form-item>
-        <el-button type="primary">主要按钮</el-button>
+        <el-button type="primary" class="login-btn">登录</el-button>
         </el-form>
   </div>
 </template>
@@ -26,5 +26,22 @@ export default {
 </script>
 
 <style>
+.login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  width: 500px;
+  padding: 30px;
+  border-radius: 5px;
+  background-color: #fff;
+}
+.login-btn {
+  margin-top: 40px;
+  width: 500px;
+}
 
 </style>
